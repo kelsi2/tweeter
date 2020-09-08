45a6b8ce5ec704ed70c882f293dd58cc77323d3d
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-08 21:49:51 +0000

    Added counter logic and color

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+//Create listener for character count (decrease count as characters are entered)
+$(document).ready(function() {
+  $(".new-tweet form textarea").on("keydown", function() {
+    const count = ($(this).val()).length;
+    const counter = $(this).closest(".new-tweet").find(".counter");
+    const maxCount = 140;
+    const remChars = maxCount - count;
+    counter.text(remChars);
+
+    if (remChars < 0) {
+      counter.addClass("negChar");
+    } else {
+      if (counter.hasClass("negChar")) {
+        counter.removeClass("negChar");
+      }
+    }
+  });
+});
