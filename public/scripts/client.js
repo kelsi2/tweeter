c41129e8d1a1b164a844a1425a0f196e84051ef5
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-09 20:05:30 +0000

    Early createTweetElement function (untested)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,26 +3,58 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//moment calculates the time since the tweet was created for the tweet element footer
+const moment = require("moment");
+//fromNow allows moment to calculate the time from the moment the tweet was created
+const fromNow = require("fromnow");
 
-const createTweetElement = function() {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-};
+$(document).ready(function() {
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  //Create HTML article for tweet post
+  const createTweetElement = tweetData => {
+    //header
+    const $header = $("<header>").addClass("tweet-header");
+    const $avatar = $("<img>", {class: "avatar", src: tweetData.user.avatars});
+    const $name = $("<h2>", {class: "name"}).text(tweetData.user.name);
+    const $handle = $("<p>", {class: "handle"}).text(tweetData.user.handle);
+    $header.append($avatar, $name, $handle);
 
-const $tweet = createTweetElement(tweetData);
+    //body
+    const $tweetText = $("<div>", {class: "tweet-text"}).text(tweetData.content.text);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    //footer
+    const $footer = $("<footer>").addClass("tweet-footer");
+    const $time = $("<div>", class {"post-date";}).text(tweetData.created_at);
+    // const $count = $("<span>", {class: "like-count"}).text(tweet.likes.length);
+    // const $like = $("<img>", {class: "like-tweet clickable hide"});
+    // const $span = $("<span>", {class: "like-text"}).text("likes");
+    $footer.append($time);
+
+    //create article
+    const $tweet = $("<article>");
+    $tweet.append($header, $tweetText, $footer);
+    return $tweet;
+  };
+
+
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.  
+
+});
