f94905bae6b230a9da8fc29877fe4b0af0cf1660
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-09 23:35:35 +0000

    add loadTweets function and debugging

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function() {
   // const createTweetElement = tweetData => {
   //   //header
@@ -91,23 +65,31 @@ $(document).ready(function() {
       $(".tweet-container").prepend(newTweet);
     });
   };
-  renderTweets(data);
 
+  //receive all tweets from server and display in feed
+  const loadTweets = () => {
+    $.ajax({
+      method: "GET",
+      url: "/tweets/",
+    }).then((response) => {
+      renderTweets(response);
+    });
+  };
 
-  //use AJAX to POST tweet to /tweets and load to page
+  //use AJAX to POST tweet to /tweets
   $(".submit-tweet").submit(function(event) {
     event.preventDefault();
 
     //serialize new tweet form data for submission to server
     const serializedData = $(this).serialize();
-    console.log(serializedData);
 
     $.post({
       url: "/tweets/",
       data: serializedData,
-
-    }).then(() => {
-      renderTweets(data);
+      success: () => {
+        $('.tweet-text').val("");
+        loadTweets();
+      }
     });
   });
 });
