9e91c6a8bd4b7c77ee94259cb961f26bf55cde83
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-10 01:15:30 +0000

    Adjust for cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,56 +5,30 @@
  */
 
 $(document).ready(function() {
-  // const createTweetElement = tweetData => {
-  //   //header
-  //   const $header = $("<header>").addClass("tweet-header");
-  //   const $avatar = $("<img>", {class: "avatar", src: tweetData.user.avatars});
-  //   const $user = $("<h2>", {class: "user"}).text(tweetData.user.name);
-  //   const $handle = $("<p>", {class: "handle"}).text(tweetData.user.handle);
-  //   $header.append($avatar, $user, $handle);
+  const createTweetElement = tweet => {
+    //header
+    const $header = $("<header>").addClass("tweet-header");
+    const $avatar = $("<img>", {class: "avatar", src: tweet.user.avatars});
+    const $user = $("<h2>", {class: "user"}).text(tweet.user.name);
+    const $handle = $("<p>", {class: "handle"}).text(tweet.user.handle);
+    $header.append($avatar, $user, $handle);
 
-  //   //body
-  //   const $tweetText = $("<div>", {class: "tweet-text"}).text(tweetData.content.text);
+    //body
+    const $tweetText = $("<div>", {id: "tweet-text"}).text(tweet.content.text);
 
-  //   //footer
-  //   const $footer = $("<footer>").addClass("tweet-footer");
-  //   const $time = $("<div>", class {"post-date";}).text(tweetData.created_at);
-  //   // const $count = $("<span>", {class: "like-count"}).text(tweet.likes.length);
-  //   // const $like = $("<img>", {class: "like-tweet clickable hide"});
-  //   // const $span = $("<span>", {class: "like-text"}).text("likes");
-  //   $footer.append($time);
+    //footer
+    const $footer = $("<footer>").addClass("tweet-footer");
+    const $time = $("<div>", {class: "post-date"}).text(tweet.created_at);
+    // var $div = $("<div>", {class: "footer-div"});
+    // var $count = $("<span>", {class: "like-count"}).text(tweet.likes.length);
+    // var $like = $("<img>", {class: "like-tweet clickable hide"});
+    // var $span = $("<span>", {class: "like-text"}).text("likes");
+    // $div.append($count, $span, $like);
+    $footer.append($time);
 
-  //   //create article
-  //   const $tweet = $(`<article class="tweet">Hello world</article>`);
-  //   $tweet.append($header, $tweetText, $footer);
-  //   return $tweet;
-  // };
-
-  const createTweetElement = (tweet) => {
-    const $tweet = $(`<article class="tweet">Hello world</article>`);
-    const htmlStr = `<article class="tweet">
-      <header class="tweet-header">
-          <div class="user">
-            <img
-              src="${tweet.user.avatars}"
-              alt="avatar"
-            />
-            <span>${tweet.user.name}</span>
-          </div>
-          <div class="handle">${tweet.user.handle}</div>
-        </header>
-        <div class="tweet-text">
-          ${tweet.content.text}
-        </div>
-        <footer class="tweet-footer">
-          <div class="post-date">${tweet.created_at}</div>
-          <span class="social icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </span>
-        </footer>`;
-    $tweet.append(htmlStr);
+    //create article
+    const $tweet = $("<article>").addClass("tweet");
+    $tweet.append($header, $tweetText, $footer);
     return $tweet;
   };
 
@@ -68,6 +42,7 @@ $(document).ready(function() {
 
   //receive all tweets from server and display in feed
   const loadTweets = () => {
+    $(".tweet-container").empty();
     $.ajax({
       method: "GET",
       url: "/tweets/",
@@ -80,13 +55,6 @@ $(document).ready(function() {
   $(".submit-tweet").submit(function(event) {
     event.preventDefault();
 
-    const textArea = $("textarea").val();
-    if (textArea === "") {
-      alert("Please enter a Tweet!");
-    } else if (textArea.length > 140) {
-      alert("Your Tweet has too many characters, please make it shorter!");
-    }
-
     //serialize new tweet form data for submission to server
     const serializedData = $(this).serialize();
 
@@ -94,8 +62,16 @@ $(document).ready(function() {
       url: "/tweets/",
       data: serializedData,
       success: () => {
-        $(".tweet-text").val("");
+        $("#tweet-text").val("");
         loadTweets();
+      },
+      error: () => {
+        const textArea = $("textarea").val();
+        if (textArea === "") {
+          alert("Please enter a Tweet!");
+        } else if (textArea.length > 140) {
+          alert("Your Tweet has too many characters, please make it shorter!");
+        }
       }
     });
   });
